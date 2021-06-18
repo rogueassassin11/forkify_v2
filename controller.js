f3d5f9e712912e037861b66534701a37bb2d3ddf
Mae Cagadas <32762531+rogueassassin11@users.noreply.github.com>
2021-06-18 23:15:47 +0800

    Update controller.js

--- controller.js
+++ controller.js
@@ -1,6 +1,33 @@
 /*********************************************/
 /*  USER STORY -> As a user, I want [action] so  that [benefit]
+    
+  MVC ARCHITECTURE
+  -> structure - organization of code
+  -> maintainability - can be easily changed
+  -> expandability - to easily add new features
+
+  COMPONENTS OF ARCHITECTURE
+  -> Business logic - code that solves the business problem
+  -> State - stores all the data about the application / single source of truth kept in sync with UI
+  -> HTTP Library - responsible for making and receiving AJAX requests
+  -> Application logic (Router) - implementation of the application
+  -> Presentation logic (UI layer) - visible part of the application / displays application state
+
+  MODEL
+  -> Business logic / state / http library
+
+  VIEW
+  -> Presentation logic
+
+  CONTROLLER
+  -> application logic / bridge between view and model
+  -> dispatches tasks to model and view
+
 /*********************************************/
+
+import icons from 'url:../img/icons.svg'; //parcel 2
+import 'core-js/stable'; //polyfill
+import 'regenerator-runtime/runtime'; //polyfill async/await
 
 const { resolveSoa } = require('dns');
 
@@ -18,10 +45,31 @@ const timeout = function (s) {
 
 ///////////////////////////////////////
 
+const renderSpinner = function (parentEl) {
+  const markup = `
+  <div class="spinner">
+    <svg>
+    <use href="${icons}#icon-loader"></use>
+    </svg>
+  </div>
+  `;
+  parentEl.innerHTML = '';
+  parentEl.insertAdjacentHTML('afterbegin', markup);
+};
+
 const showRecipe = async function () {
   try {
+    //get ID from the hash
+    const id = window.location.hash.slice(1);
+
+    //guard clause if there is no ID from hash
+    if (!id) return;
+
+    // 1. Loading recipe
+    renderSpinner(recipeContainer);
+
     const res = await fetch(
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
 
     const data = await res.json();
@@ -40,9 +88,118 @@ const showRecipe = async function () {
       ingredients: recipe.ingredients,
     };
     console.log(recipe);
+
+    //2. Rendering recipe
+
+    const markup = `
+    <figure class="recipe__fig">
+    <img src="${recipe.image}" alt="Tomato" class="recipe__img" />
+    <h1 class="recipe__title">
+      <span>${recipe.title}</span>
+    </h1>
+  </figure>
+
+  <div class="recipe__details">
+    <div class="recipe__info">
+      <svg class="recipe__info-icon">
+        <use href="${icons}#icon-clock"></use>
+      </svg>
+      <span class="recipe__info-data recipe__info-data--minutes">${
+        recipe.cookingTime
+      }</span>
+      <span class="recipe__info-text">minutes</span>
+    </div>
+    <div class="recipe__info">
+      <svg class="recipe__info-icon">
+        <use href="${icons}#icon-users"></use>
+      </svg>
+      <span class="recipe__info-data recipe__info-data--people">${
+        recipe.servings
+      }</span>
+      <span class="recipe__info-text">servings</span>
+
+      <div class="recipe__info-buttons">
+        <button class="btn--tiny btn--increase-servings">
+          <svg>
+            <use href="${icons}#icon-minus-circle"></use>
+          </svg>
+        </button>
+        <button class="btn--tiny btn--increase-servings">
+          <svg>
+            <use href="${icons}#icon-plus-circle"></use>
+          </svg>
+        </button>
+      </div>
+    </div>
+
+    <div class="recipe__user-generated">
+      <svg>
+        <use href="${icons}#icon-user"></use>
+      </svg>
+    </div>
+    <button class="btn--round">
+      <svg class="">
+        <use href="${icons}#icon-bookmark-fill"></use>
+      </svg>
+    </button>
+  </div>
+
+  <div class="recipe__ingredients">
+    <h2 class="heading--2">Recipe ingredients</h2>
+    <ul class="recipe__ingredient-list">
+
+    ${recipe.ingredients
+      .map(ing => {
+        return `
+      <li class="recipe__ingredient">
+      <svg class="recipe__icon">
+        <use href="${icons}#icon-check"></use>
+      </svg>
+      <div class="recipe__quantity">${ing.quantity}</div>
+      <div class="recipe__description">
+        <span class="recipe__unit">${ing.unit}</span>
+        ${ing.description}
+      </div>
+    </li>`;
+      })
+      .join('')}
+    </ul>
+  </div>
+
+  <div class="recipe__directions">
+    <h2 class="heading--2">How to cook it</h2>
+    <p class="recipe__directions-text">
+      This recipe was carefully designed and tested by
+      <span class="recipe__publisher">${
+        recipe.publisher
+      }</span>. Please check out
+      directions at their website.
+    </p>
+    <a
+      class="btn--small recipe__btn"
+      href="${recipe.sourceUrl}"
+      target="_blank"
+    >
+      <span>Directions</span>
+      <svg class="search__icon">
+        <use href="${icons}#icon-arrow-right"></use>
+      </svg>
+    </a>
+  </div>
+    `;
+
+    //empty the container
+    recipeContainer.innerHTML = '';
+
+    //insert the markup
+    recipeContainer.insertAdjacentHTML('afterbegin', markup);
   } catch (err) {
     console.error(err);
   }
 };
 
-showRecipe();
+//hash change event and load event listener
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
+
+// window.addEventListener('hashchange', showRecipe);
+// window.addEventListener('hashchange', showRecipe);
