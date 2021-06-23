52d298a1f5484c1a1c7683442790b24b37ab565c
Mae Cagadas <32762531+rogueassassin11@users.noreply.github.com>
2021-06-23 22:34:44 +0800

    Update model.js

--- model.js
+++ model.js
@@ -2,13 +2,18 @@ import { async } from 'regenerator-runtime';
 import { API_URL } from './config.js';
 import { getJSON } from './helpers.js';
 
+//state contains all the data
 export const state = {
   recipe: {},
+  search: {
+    query: '',
+    results: []
+  }
 };
 
 export const loadRecipe = async function (id) {
   try {
-    const data = await getJSON(`${API_URL}/${id}`);
+    const data = await getJSON(`${API_URL}${id}`);
 
     const { recipe } = data.data;
     state.recipe = {
@@ -24,5 +29,25 @@ export const loadRecipe = async function (id) {
     console.log(state.recipe);
   } catch (err) {
     console.error(`${err} !!!`);
+    throw err;
+  }
+};
+
+export const loadSearchResults = async function(query) {
+  try {
+    state.search.query = query;
+    const data = await getJSON(`${API_URL}?search=${query}`);
+    console.log(data);
+
+    state.search.results = data.data.recipes.map(rec => {
+      return {id: rec.id,
+      title: rec.title,
+      publisher: rec.publisher,
+      image: rec.image_url};
+
+    }) 
+  } catch (err) {
+    console.error(`${err} !!!`);
+    throw err;
   }
 };
