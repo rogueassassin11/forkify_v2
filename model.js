de5a0aac61808b6c997da694f834b96429ffc1f5
Mae Cagadas <32762531+rogueassassin11@users.noreply.github.com>
2021-06-25 22:53:32 +0800

    Update model.js

--- model.js
+++ model.js
@@ -1,5 +1,5 @@
 import { async } from 'regenerator-runtime';
-import { API_URL } from './config.js';
+import { API_URL, RES_PER_PAGE } from './config.js';
 import { getJSON } from './helpers.js';
 
 //state contains all the data
@@ -7,8 +7,10 @@ export const state = {
   recipe: {},
   search: {
     query: '',
-    results: []
-  }
+    results: [],
+    page: 1,
+    resultsPerPage: 10,
+  },
 };
 
 export const loadRecipe = async function (id) {
@@ -33,21 +35,30 @@ export const loadRecipe = async function (id) {
   }
 };
 
-export const loadSearchResults = async function(query) {
+export const loadSearchResults = async function (query) {
   try {
     state.search.query = query;
     const data = await getJSON(`${API_URL}?search=${query}`);
     console.log(data);
 
     state.search.results = data.data.recipes.map(rec => {
-      return {id: rec.id,
-      title: rec.title,
-      publisher: rec.publisher,
-      image: rec.image_url};
-
-    }) 
+      return {
+        id: rec.id,
+        title: rec.title,
+        publisher: rec.publisher,
+        image: rec.image_url,
+      };
+    });
   } catch (err) {
     console.error(`${err} !!!`);
     throw err;
   }
+};
+
+export const getSearchResultsPage = function (page = state.search.page) {
+  state.search.page = page;
+  const start = (page - 1) * state.search.resultsPerPage; // 0;
+  const end = page * state.search.resultsPerPage; // 9;
+
+  return state.search.results.slice(start, end);
 };
