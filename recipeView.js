4faf25c99b82effa7a0529ebd5114fa40be38ec1
Mae Cagadas <32762531+rogueassassin11@users.noreply.github.com>
2021-07-01 23:30:49 +0800

    Update recipeView.js

--- recipeView.js
+++ recipeView.js
@@ -16,15 +16,23 @@ class RecipeView extends View {
   }
 
   addHandlerUpdateServings(handler) {
-    this._parentElement.addEventListener('click', function(e) {
-      const btn = e.target.closest('.btn--update-servings')
+    this._parentElement.addEventListener('click', function (e) {
+      const btn = e.target.closest('.btn--update-servings');
       if (!btn) return;
-     
+
       // read data from dataset-update-to
       const updateTo = btn.dataset.updateTo;
 
       if (+updateTo > 0) handler(+updateTo);
-    })
+    });
+  }
+
+  addHandlerAddBookmark(handler) {
+    this._parentElement.addEventListener('click', function (e) {
+      const btn = e.target.closest('.btn--bookmark');
+      if (!btn) return;
+      handler();
+    });
   }
 
   _generateMarkup() {
@@ -57,15 +65,15 @@ class RecipeView extends View {
 
       <div class="recipe__info-buttons">
         <button class="btn--tiny btn--update-servings" data-update-to="${
-        this._data.servings - 1
-      }">
+          this._data.servings - 1
+        }">
           <svg>
             <use href="${icons}#icon-minus-circle"></use>
           </svg>
         </button>
         <button class="btn--tiny btn--update-servings" data-update-to="${
-        this._data.servings + 1
-      }">
+          this._data.servings + 1
+        }">
           <svg>
             <use href="${icons}#icon-plus-circle"></use>
           </svg>
@@ -75,9 +83,11 @@ class RecipeView extends View {
 
     <div class="recipe__user-generated">
     </div>
-    <button class="btn--round">
+    <button class="btn--round btn--bookmark">
       <svg class="">
-        <use href="${icons}#icon-bookmark-fill"></use>
+        <use href="${icons}#icon-bookmark${
+      this._data.bookmarked ? '-fill' : ''
+    }"></use>
       </svg>
     </button>
   </div>
