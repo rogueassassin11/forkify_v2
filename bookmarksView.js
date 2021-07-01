566483cb85d15b13c76703ea9051679abf445446
Mae Cagadas <32762531+rogueassassin11@users.noreply.github.com>
2021-07-01 23:31:12 +0800

    Add files via upload

---
+++ bookmarksView.js
@@ -0,0 +1,20 @@
+//import parent class View
+import View from './View.js';
+import previewView from './previewView.js';
+//import icons
+import icons from 'url:../../img/icons.svg';
+
+class BookmarksView extends View {
+  _parentElement = document.querySelector('.bookmarks__list');
+  _errorMessage = 'No bookmarks yet. Find a nice recipe and bookmark it! :)';
+  _message = '';
+
+  _generateMarkup() {
+    console.log(this._data);
+    return this._data
+      .map(bookmark => previewView.render(bookmark, false))
+      .join('');
+  }
+}
+
+export default new BookmarksView();
