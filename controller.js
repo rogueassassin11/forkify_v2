e0080e193c0f42b656d02da8d8a1b03b4f7b60ec
Mae Cagadas <32762531+rogueassassin11@users.noreply.github.com>
2021-07-01 23:29:03 +0800

    Update controller.js

--- controller.js
+++ controller.js
@@ -41,6 +41,7 @@ import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
 import paginationView from './views/paginationView.js';
+import bookmarksView from './views/bookmarksView.js';
 
 import 'core-js/stable'; //polyfill
 import 'regenerator-runtime/runtime'; //polyfill async/await
@@ -69,6 +70,7 @@ const controlRecipes = async function () {
 
     // 0) Update results view to mark selected search result
     resultsView.update(model.getSearchResultsPage());
+    bookmarksView.update(model.state.bookmarks);
 
     // 1. Loading recipe
     await model.loadRecipe(id);
@@ -124,9 +126,26 @@ const controlServings = function (newServings) {
   recipeView.update(model.state.recipe);
 };
 
+// adding new bookmark
+const controlAddBookmark = function () {
+  // 1. Add or remove bookmark
+  if (!model.state.recipe.bookmarked) {
+    model.addBookmark(model.state.recipe);
+  } else {
+    model.deleteBookmark(model.state.recipe.id);
+  }
+
+  // 2. Update recipe view
+  recipeView.update(model.state.recipe);
+
+  // 3. Render bookmarks
+  bookmarksView.render(model.state.bookmarks);
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
   recipeView.addHandlerUpdateServings(controlServings);
+  recipeView.addHandlerAddBookmark(controlAddBookmark);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
 };
